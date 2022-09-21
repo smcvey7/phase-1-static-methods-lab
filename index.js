087b84149e8cb120360bff48c38a4c9620883551
Steven McVey <smcvey1210@gmail.com>
2022-09-20 17:48:30 -0700

    commit tree d622a1079ab7b45c5052aaf057998b6e4d68756e

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let array = string.split(' ')
+    let final = ''
+    array[0] = this.capitalize(array[0])
+    array.forEach(word => {
+      const test = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].indexOf(word)
+      if(test === -1){
+        final += `${this.capitalize(word)} `
+      }else{
+      final += `${word} `
+      }
+    })
+    final = final.slice(0, -1)
+    return final
+  }
 }
